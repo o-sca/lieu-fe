9a28ddfef35bc506a4eb7cc7336a47b256402694
oscar <97479266+o-sca@users.noreply.github.com>
2023-11-12 16:35:44 -0800

    refactor(landing): create tab for uploading and paragraph

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
     CommonModule,
+    MatCardModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
