da8e6feadfb2cd7d7ccf3cd3b12fc195db948826
oscar <97479266+o-sca@users.noreply.github.com>
2023-11-15 18:52:56 -0800

    feat: auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,12 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+export const AuthGuard: CanActivateFn = () => {
+  const router = inject(Router);
+  const auth = inject(AuthService);
+  if (auth.authenticated) {
+    return true;
+  }
+  return router.createUrlTree(['signin']);
+};
