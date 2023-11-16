e1b5b8bab9ba5a63c938468e57ee4ac63b38fa80
Oscar Zhu <ozhu35@gmail.com>
2023-11-15 22:32:17 -0800

    refactor(header): signout button to the absolute right and hide signup and signin after logged in

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   imports: [
+    AsyncPipe,
     RouterLinkActive,
     RouterLink,
     NgIf,
@@ -18,6 +20,30 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   standalone: true,
 })
-export class HeaderComponent {
-  authenticated$ = inject(AuthService).authenticated;
+export class HeaderComponent implements OnInit, OnDestroy {
+  private _subscription: Subscription = {} as Subscription;
+  authenticated = false;
+
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this._subscription = this.auth.authChanged.subscribe(
+      (status) => {
+        this.authenticated = status;
+      },
+      (err) => console.error(err),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
+
+  signOut(): void {
+    this.auth.signOut();
+    this.router.navigate(['/']);
+  }
 }
