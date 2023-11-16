a20aa0ab5e8538eda42ec369a1691f4697d5dd6f
Oscar Zhu <ozhu35@gmail.com>
2023-11-15 22:39:26 -0800

    refactor(footer): keep sticky bottom

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-layout-footer',
   templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css',
   imports: [],
   standalone: true,
 })
