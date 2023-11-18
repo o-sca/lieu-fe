31f9fc95d63fd02e42c16f449e2eb0d01756db2d
oscar <97479266+o-sca@users.noreply.github.com>
2023-11-17 17:43:58 -0800

    refactor(profile): show profile details on profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileService } from '../core/services/profile.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit, OnDestroy {
+  name: string;
+  email: string;
+  userType: string;
 
+  constructor(private profile: ProfileService) {
+    this.name = '';
+    this.email = '';
+    this.userType = '';
+  }
+
+  ngOnInit(): void {
+    this.profile.getProfile().subscribe({
+      next: (response) => {
+        this.name = response['name'];
+        this.email = response['email'];
+        this.userType = response['user_type'];
+      },
+      error(err) {
+        console.error(err);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    return;
+  }
 }
